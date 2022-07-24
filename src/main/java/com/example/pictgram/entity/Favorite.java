c4dd3a5e6ff1055a37743c928045e2d8a9f1190e
tomohiro <tomohiro0123456789@gmail.com>
2022-07-24 18:24:59 +0900

    25

--- src/main/java/com/example/pictgram/entity/Favorite.java
+++ src/main/java/com/example/pictgram/entity/Favorite.java
@@ -1,7 +1,6 @@
 package com.example.pictgram.entity;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,21 +8,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "topic")
+@Table(name = "favorite")
 @Data
-public class Topic extends AbstractEntity implements Serializable {
+public class Favorite extends AbstractEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@SequenceGenerator(name = "topic_id_seq")
+	@SequenceGenerator(name = "favorite_id_seq")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
@@ -31,22 +29,9 @@ public class Topic extends AbstractEntity implements Serializable {
 	private Long userId;
 
 	@Column(nullable = false)
-	private String path;
+	private Long topicId;
 
-	@Column(nullable = false, length = 1000)
-	private String description;
-
-	@Column
-	private Double latitude;
-
-	@Column
-	private Double longitude;
-
-	@OneToOne
-	@JoinColumn(name = "userId", insertable = false, updatable = false)
-	private User user;
-
-	@OneToMany
+	@ManyToOne
 	@JoinColumn(name = "topicId", insertable = false, updatable = false)
-	private List<Favorite> favorites;
+	private Topic topic;
 }
